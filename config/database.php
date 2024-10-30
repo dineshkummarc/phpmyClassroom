1d7a1f15fd51e1f3213e17c3c2ac662ccb36d70c
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-30 15:38:22 +0530

    update

--- config/database.php
+++ config/database.php
@@ -44,11 +44,11 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'mysqli',
+            'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE', 'phpmyclassroom'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
